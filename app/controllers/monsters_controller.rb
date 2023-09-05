b00f27cc2e3ffea3a376c3c49da2803a65638de1
slink2647@gmail.com <32804454+sarah-link@users.noreply.github.com>
2023-09-05 16:23:56 -0500

    create monsters#show

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -3,4 +3,20 @@ class MonstersController < ApplicationController
   def index
     render json: Monster.all
   end
+
+  def create
+
+  end
+
+  def show
+    render json: Monster.find(params[:id])
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
